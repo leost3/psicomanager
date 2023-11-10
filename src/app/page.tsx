6e0c99b3cef8445b3d877cd8ee282470ccff87c2
Leo <lstudart@pyx4.com>
2023-11-10 16:36:37 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d0ced3] to-[#6f00ff] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          PsicoManager
+        </h1>
+      </div>
+    </main>
+  );
+}
+
