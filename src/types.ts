9ed2253f8be02ec0f8d9d12989646e96011959ca
Leo <lstudart@pyx4.com>
2023-11-11 13:21:02 -0500

    feat: add global types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+  /** An ISO 8601-encoded datetime */
+  ISO8601DateTime: string
+}
+
+
+export type ID = Scalars['ID']
